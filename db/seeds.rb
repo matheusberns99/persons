363ebac385317360e466b1eee7c7cd9fc8fd1a98
= <=>
2025-03-24 15:39:55 -0300

    feat: added user creation in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+user = User.new(email: "admin@4asset.com")
+
+password = "Senha123"
+
+user.password = password
+user.password_confirmation = password
+
+user.save
